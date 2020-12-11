237e78cc9bee4a06f7c9aa63e2c70f600595819c
TakekazuKATO <kato.takekazu@sist.ac.jp>
2020-12-11 14:28:03 +0900

    Update TailRec.jl
    Support latest version of julia 1.5.3

--- src/TailRec.jl
+++ src/TailRec.jl
@@ -2,7 +2,7 @@ module TailRec
 export @tailrec
 
 macro tailrec(func)
-    fargs=map(e->isa(e,Expr)?e.args[1]:e,func.args[1].args)
+    fargs=map(e->if isa(e,Expr) e.args[1] else e end,func.args[1].args)
     fbody=func.args[2]
     fbody=rewrite(fbody,fargs)
     func.args[2]=Expr(:block,:(@label retry),fbody)
@@ -14,7 +14,7 @@ function rewrite(expr,args,callflag=false)
         expr
     elseif expr.head == :call && expr.args[1] == args[1]
         if callflag
-            warn("Not tail recursive call is found.")
+            @warn "Not tail recursive call is found."
             expr
         else
             newargs=Expr(:tuple)
@@ -24,10 +24,10 @@ function rewrite(expr,args,callflag=false)
             Expr(:block, Expr(:(=),newargs,oldargs), :(@goto retry) )
         end
     elseif expr.head == :block
-        expr.args[end]=rewrite(expr.args[end],args,expr.head==:call? true:callflag)
+        expr.args[end]=rewrite(expr.args[end],args, expr.head==:call || callflag)
         expr
     else
-        expr.args = map(a->rewrite(a,args,expr.head==:call? true:callflag),expr.args)
+        expr.args = map(a->rewrite(a,args, expr.head==:call ||callflag),expr.args)
         expr
     end
 end
